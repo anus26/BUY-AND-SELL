a6649542b93c5ef7406e22383e4e321c0ea09d08
anas-raza~ <anusraza268@gmail.com>
2024-08-22 10:30:08 -0700

    modified

--- home.js
+++ home.js
@@ -1,5 +1,10 @@
 import {  onAuthStateChanged ,signOut } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js"; 
 import { auth } from "./config.js";
+import { db } from "./config.js";
+
+const logout=document.querySelector('#logout-btn')
+
 
 
 
@@ -10,7 +15,6 @@ onAuthStateChanged(auth, (user) => {
     // https://firebase.google.com/docs/reference/js/auth.user
     const uid = user.uid;
     console.log(uid);
-    window.location='index.html'
   } else {
     console.log(error);
   }
@@ -18,14 +22,35 @@ onAuthStateChanged(auth, (user) => {
 
 
 
-// const cardbody =document.querySelector('.card-body');
-const logout=document.querySelector('#logout-btn')
-
 logout.addEventListener('click',()=>{
-    signOut(auth).then(() => {
-        console.log('logout successfully');
-        window.location='index.html'
-      }).catch((error) => {
-        console.log(error);
-      });
+  signOut(auth).then(() => {
+    console.log('logout successfully');
+    window.location='index.html'
+  }).catch((error) => {
+    console.log(error);
+  });
 })
+
+const form =document.querySelector('#form')
+const product =document.querySelector('#product')
+const productdescripation =document.querySelector('#product-descripation')
+const price =document.querySelector('#price')
+const  number=document.querySelector('#number')
+const name=document.querySelector('#name')
+  
+form.addEventListener('submit',async(event)=>{
+    event.preventDefault();
+  
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        product:product.value,
+        productdescripation:productdescripation.value,
+        price:price.value,
+        number:number.value,
+        name:name.value
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  })
